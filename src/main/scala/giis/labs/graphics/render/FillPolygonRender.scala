a440b9daeab001f32f3aa167c61878ba965c23af
Yasko Alexei <alexei.yasko@gmail.com>
2012-10-25 17:30:47 +0300

    fill polygon changed (not worked)

--- src/main/scala/giis/labs/graphics/render/FillPolygonRender.scala
+++ src/main/scala/giis/labs/graphics/render/FillPolygonRender.scala
@@ -1,6 +1,6 @@
 package giis.labs.graphics.render
 
-import giis.labs.model.shape.{Shape, Line, FillPolygon}
+import giis.labs.model.shape.{Line, Polygon, Shape, FillPolygon}
 import giis.labs.graphics.{Pixel, DrawingContext}
 import giis.labs.model.Point
 
@@ -8,152 +8,137 @@ import giis.labs.model.Point
  * @author Q-YAA
  */
 class FillPolygonRender(
-    fillPolygon: FillPolygon, drawingContext: DrawingContext, render: Render) extends Render(fillPolygon, drawingContext) {
+    fillPolygon: FillPolygon, drawingContext: DrawingContext, polygon: Polygon) extends Render(fillPolygon, drawingContext) {
 
     /**
      * Template method for the shape drawing. Concrete renders must override this method.
      *
      * @return List[Pixel] result of drawing
      */
-    def drawShape = {
+    def drawShape = fillPolygon.shapeType match {
+        case Shape.FillPolygonByLine => fillByLine
+    }
 
+    private def fillByLine: List[Pixel] = {
+        val sortedByY = polygon.getPointList.sortBy[Int](p => p.y)
+        val sortedByX = polygon.getPointList.sortBy[Int](p => p.x)
+
+        val maxX = sortedByX.reverse.head.x
+        val minX = sortedByX.head.x
+        val maxY = sortedByY.reverse.head.y
+        val minY = sortedByY.head.y
+
+        var rowIntersections = List[(Point, Boolean)]()
+        for (i <- minY to maxY) {
+            rowIntersections = rowIntersections ::: findRowIntersections(i, minX, maxX)
+        }
+
+        val sortedIntervalList =
+            rowIntersections.sortWith((p1, p2) => p1._1.y < p2._1.y || (p1._1.y == p2._1.y && p1._1.x <= p2._1.x))
+
+        System.out.println(sortedIntervalList)
+        System.out.println("\n\n")
+
+        val filteredIntervalList = removeWastePointsFromIntervalList(sortedIntervalList)
+
+        drawIntervalList(filteredIntervalList)
+    }
+
+    private def drawIntervalList(intervalList: List[Point]): List[Pixel] = {
         var resultPixelList = List[Pixel]()
 
-        System.out.println(render.drawShape)
+        for (i <- 0 until intervalList.length / 2) {
+            val firstIntervalPoint = intervalList(i * 2)
+            val secondIntervalPoint = intervalList(i * 2 + 1)
 
-        val sortedByY = render.drawShape.sortBy[Int](p => p.point.y)
-        val sortedByX = sortedByY.sortBy[Int](p => p.point.x)
+            System.out.println(firstIntervalPoint + "   " + secondIntervalPoint)
 
-        val maxX = sortedByX.reverse.head.point.x
-        val minX = sortedByX.head.point.x
+            for (j <- (firstIntervalPoint.x + 1) until secondIntervalPoint.x) {
+                val newPixel = Pixel.createPixel(j, firstIntervalPoint.y, drawingContext.fillColor)
 
-        val maxY = sortedByY.reverse.head.point.y
-        val minY = sortedByY.head.point.y
-
-        for (i <- minY to maxY) {
-            var drawCondition = false
-            var isOtherLine = false
-
-            for (j <- minX to maxX) {
-                val pixels = sortedByX.filter(p => p.point.y == i && p.point.x == j)
-                val previousPixels = sortedByX.find(p => p.point.y == i && p.point.x == j - 1)
-
-                drawCondition = resolveDrawCondition(drawCondition, pixels)
-
-                if (!pixels.isEmpty && previousPixels.isEmpty) {
-                    isOtherLine = !isOtherLine
-                }
-
-                for (i <- 1 until pixels.length) {
-                    isOtherLine = !isOtherLine
-                }
-
-                if (pixels.isEmpty && isOtherLine) {
-                    resultPixelList = Pixel.createPixel(j, i, drawingContext.fillColor) :: resultPixelList
+                if (!polygon.createRender(drawingContext).drawShape.exists(p => p.point == newPixel.point)) {
+                    resultPixelList = newPixel :: resultPixelList
                 }
             }
-
         }
 
-//        val polygonLines = createPolygonLines(render.shape)
-//
-//        for (i <- minY to maxY) {
-//            val intervalList = createIntervals(i, maxX, minX, polygonLines)
-//
-//            for (k <- 1 until intervalList.length) {
-//                val left = intervalList.reverse(k - 1)
-//                val right = intervalList.reverse(k)
-//
-//                if (left.x + 1 != right.x) {
-//                    resultPixelList =
-//                        resultPixelList ::: drawPixelsFromInterval(left, right)
-//                }
-//            }
-//        }
+        System.out.println("\n\n")
 
-        resultPixelList
+        resultPixelList.reverse
     }
 
-    private def resolveDrawCondition(currentDrawCondition: Boolean, currentPixels: List[Pixel]): Boolean = {
-        var drawCondition = currentDrawCondition
+    private def removeWastePointsFromIntervalList(sortedIntervalList: List[(Point, Boolean)]): List[Point] = {
+        var resultIntervalList = List[Point]()
 
-        for (i <- 0 until currentPixels.length) {
-            drawCondition = !drawCondition
-        }
+        for (i <- 1 until sortedIntervalList.length) {
+            val firstElement = sortedIntervalList(i - 1)
+            val secondElement = sortedIntervalList(i)
 
-        drawCondition
-    }
-
-    private def drawPixelsFromInterval(left: Point, right: Point): List[Pixel] = {
-        (for (i <- (left.x + 1) until right.x) yield {
-            Pixel.createPixel(i, left.y, drawingContext.fillColor)
-        }).toList
-    }
-
-    private def createPolygonLines(shape: Shape): Array[LineRender] = {
-        val shapePointArray = shape.getPointList.toArray
-        val lines = Array.ofDim[LineRender](shapePointArray.length)
-
-        for (i <- 1 until shapePointArray.length) {
-            val line = new Line(shapePointArray(i - 1), shapePointArray(i)) {
-                def shapeType = Shape.LineBrezenhem
+            if (firstElement._1 == secondElement._1 && firstElement._2) {
+                //resultIntervalList = resultIntervalList ::: List[Point](firstElement._1)
             }
-
-            lines(i - 1) = new LineRender(line, DrawingContext.createDrawingContext)
-        }
-
-        val line = new Line(shapePointArray.last, shapePointArray.head) {
-            def shapeType = Shape.LineBrezenhem
-        }
-
-        lines(shapePointArray.length - 1) = new LineRender(line, DrawingContext.createDrawingContext)
-
-        lines
-    }
-
-    private def createIntervals(row: Int, maxX: Int, minX: Int, lines: Array[LineRender]): List[Point] = {
-        var intervals = List[Point]()
-
-        for (i <- 1 until lines.length) {
-            var firstPixel: Point = null
-            var secondPixel: Point = null
-
-            val firstLine = lines(i - 1).drawShape
-            val secondLine = lines(i).drawShape
-
-            for (j <- minX to maxX) {
-                val first = firstLine.find(p => p.point.y == row && p.point.x == j)
-
-                if (!first.isEmpty) {
-                    firstPixel = first.head.point
-                }
-
-                val second = secondLine.find(p => p.point.y == row && p.point.x == j)
-
-                if (!second.isEmpty) {
-                    secondPixel = second.head.point
-                }
-            }
-
-            if (firstPixel != null && secondPixel != null) {
-                intervals = intervals ::: List(firstPixel, secondPixel)
+            else {
+                resultIntervalList = resultIntervalList ::: List[Point](firstElement._1)
             }
         }
 
-        //        for (i <- minX to maxX) {
-        //
-        //            for (j <- 1 until lines.length) {
-        //                val firstPixels = line(j - 1).drawShape.find(p => p.point.y == row && p.point.x == i)
-        //                val secondPixels
-        //                if (!pixels.isEmpty) {
-        //                    intervals = pixels.head.point :: intervals
-        //                }
-        //            }
-        //
-        //            //val nextPixels = sortedByX.find(p => p.point.y == i && p.point.x == j + 1)
-        //            //val previousPixels = sortedByX.find(p => p.point.y == i && p.point.x == j - 1)
-        //        }
+        resultIntervalList
+    }
 
-        intervals
+    private def findRowIntersections(row: Int, minX: Int, maxX: Int): List[(Point, Boolean)] = {
+        var intersectionList = List[(Point, Boolean)]()
+
+        for (edge <- polygon.getEdgeList if edge.getPointList(0).y != edge.getPointList(1).y) {
+
+            var intersectionPoint: Point = null
+            val edgePixels: List[Pixel] = edge.createRender(drawingContext).drawShape
+
+            for (i <- minX to maxX if edgePixels.exists(p => p.point.y == row && p.point.x == i) && intersectionPoint == null) {
+                intersectionPoint = new Point(i, row)
+            }
+
+            val isVertex = intersectionPoint != null && polygon.getPointList.contains(intersectionPoint)
+            val isVertexInLocalMinOrMax = isVertex && this.isVertexInLocalMinOrMax(intersectionPoint, polygon.getEdgeList)
+
+            if (isVertexInLocalMinOrMax) {
+                intersectionList = (intersectionPoint, false) :: intersectionList
+            }
+            else if (isVertex) {
+                intersectionList = (intersectionPoint, true) :: intersectionList
+            }
+            else if (intersectionPoint != null) {
+                intersectionList = (intersectionPoint, false) :: intersectionList
+            }
+        }
+
+        intersectionList
+    }
+
+    private def isVertexInLocalMinOrMax(vertex: Point, edgeList: List[Line]): Boolean = {
+        val connectedEdges = edgeList.filter(edge => edge.getPointList.contains(vertex))
+
+        val firstEdgeVertex = getOtherVertex(vertex, connectedEdges(0))
+        val secondEdgeVertex = getOtherVertex(vertex, connectedEdges(1))
+
+        if ((firstEdgeVertex.y > vertex.y && secondEdgeVertex.y > vertex.y) ||
+            (firstEdgeVertex.y < vertex.y && secondEdgeVertex.y < vertex.y)) {
+
+            true
+        }
+        else {
+            false
+        }
+    }
+
+    private def getOtherVertex(vertex: Point, edge: Line): Point = {
+        if (edge.getPointList(0) == vertex) {
+            edge.getPointList(1)
+        }
+        else if (edge.getPointList(1) == vertex) {
+            edge.getPointList(0)
+        }
+        else {
+            null
+        }
     }
 }
