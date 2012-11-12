aa760edbb074e31843977a42a99842d57323f3c0
Yasko Alexei <alexei.yasko@gmail.com>
2012-11-12 08:28:40 +0300

    style fixed

--- src/main/scala/giis/labs/utils/Clipping.scala
+++ src/main/scala/giis/labs/utils/Clipping.scala
@@ -12,14 +12,14 @@ import java.awt.Color
 class Clipping(graphicsScene: GraphicsScene) {
 
     def clip(polygonList: List[Point], pointList: List[Point]) {
-        var point1 = pointList.head
-        var point2 = pointList.tail.head
+        val point1 = pointList.head
+        val point2 = pointList.tail.head
 
         var listLine = List[giis.labs.model.Point]()
         listLine = point1 :: listLine
         listLine = point2 :: listLine
 
-        var shapeLine = ShapeFactory.createShape(listLine, Shape.LineBrezenhem)
+        val shapeLine = ShapeFactory.createShape(listLine, Shape.LineBrezenhem)
         DrawingContext.color_=(Color.LIGHT_GRAY)
         graphicsScene.addShapeRender(shapeLine.createRender(DrawingContext.createDrawingContext))
 
@@ -30,15 +30,15 @@ class Clipping(graphicsScene: GraphicsScene) {
         var t2: Float = 1
 
         // compute the direction vector
-        var dirV = new PointFloat(point2.x - point1.x, point2.y - point1.y)
+        val dirV = new PointFloat(point2.x - point1.x, point2.y - point1.y)
         var visible = true
         var i = 0
 
         while (i < polygonList.size && visible) {
 
-            var q0 = new PointFloat(point1.x - polygonArray.apply(i).x, point1.y - polygonArray.apply(i).y)
-            var qi = dotProduct(normals.apply(i), q0)
-            var pi = dotProduct(normals.apply(i), dirV)
+            val q0 = new PointFloat(point1.x - polygonArray.apply(i).x, point1.y - polygonArray.apply(i).y)
+            val qi = dotProduct(normals.apply(i), q0)
+            val pi = dotProduct(normals.apply(i), dirV)
 
             if (pi == 0) {
                 // Parallel or Point
@@ -46,10 +46,11 @@ class Clipping(graphicsScene: GraphicsScene) {
                 // intersections with this side
                 // but there may be with other edges, so in this case just keep going
                 if (qi > 0) {
-                    visible = false //   Parallel and outside or point (p1 == p2) and outside
+                    //   Parallel and outside or point (p1 == p2) and outside
+                    visible = false
                 }
             } else {
-                var t = -(qi.toFloat / pi.toFloat)
+                val t = -(qi.toFloat / pi.toFloat)
                 if (pi < 0) {
                     // entering
                     if (t > t1) {
@@ -66,14 +67,17 @@ class Clipping(graphicsScene: GraphicsScene) {
         }
 
         if (t1 <= t2 && t1 >= 0 && t2 <= 1) {
-            var p11 = new Point(math.round(point1.x.toFloat + t1 * dirV.x.toFloat), math.round(point1.y.toFloat + t1 * dirV.y.toFloat))
-            var p22 = new Point(math.round(point1.x.toFloat + t2 * dirV.x.toFloat), math.round(point1.y.toFloat + t2 * dirV.y.toFloat))
+            val p11 =
+                new Point(math.round(point1.x.toFloat + t1 * dirV.x.toFloat), math.round(point1.y.toFloat + t1 * dirV.y.toFloat))
+
+            val p22 =
+                new Point(math.round(point1.x.toFloat + t2 * dirV.x.toFloat), math.round(point1.y.toFloat + t2 * dirV.y.toFloat))
 
             var listLine = List[giis.labs.model.Point]()
             listLine = p11 :: listLine
             listLine = p22 :: listLine
 
-            var shapeLine = ShapeFactory.createShape(listLine, Shape.LineBrezenhem)
+            val shapeLine = ShapeFactory.createShape(listLine, Shape.LineBrezenhem)
             DrawingContext.color_=(Color.BLACK)
             graphicsScene.addShapeRender(shapeLine.createRender(DrawingContext.createDrawingContext))
         } else {
@@ -88,8 +92,11 @@ class Clipping(graphicsScene: GraphicsScene) {
             val j = (i + 1) % polygonList.length
             val k = (i + 2) % polygonList.length
 
-            val p1 = new PointFloat(-(polygonList.apply(j).y.toFloat - polygonList.apply(i).y.toFloat) / (polygonList.apply(j).x.toFloat - polygonList.apply(i).x.toFloat), 1)
-            val v1 = new PointFloat(polygonList.apply(k).x.toFloat - polygonList.apply(i).x.toFloat, polygonList.apply(k).y.toFloat - polygonList.apply(i).y.toFloat)
+            val p1 = new PointFloat(-(polygonList.apply(j).y.toFloat - polygonList.apply(i).y.toFloat) /
+                (polygonList.apply(j).x.toFloat - polygonList.apply(i).x.toFloat), 1)
+
+            val v1 = new PointFloat(polygonList.apply(k).x.toFloat - polygonList.apply(i).x.toFloat,
+                polygonList.apply(k).y.toFloat - polygonList.apply(i).y.toFloat)
 
             // inner normal
             var koef: Float = 0
